0023ca8ee4bbd1e6ba256d24dbbf8b6a1723ea80
Portian02 <135663498+Portian02@users.noreply.github.com>
2024-01-19 09:36:33 -0600

    just adding the branch to the tree in  github

--- fwd_api_intranet/app/models/calendar_event.rb
+++ fwd_api_intranet/app/models/calendar_event.rb
@@ -1,8 +1,7 @@
+# En tu modelo app/models/calendar_event.rb
 class CalendarEvent < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 255 }
-  validates :description, presence: true
-  validates :document, presence: true # Puedes ajustar esta validación según tus necesidades
-
-
-
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :description, length: { maximum: 1000 }
+  validates :url, url_format: { allow_blank: true }
 end
+
--- fwd_api_intranet/app/models/user.rb
+++ fwd_api_intranet/app/models/user.rb
@@ -10,11 +10,16 @@ class User < ApplicationRecord
 
 
   enum role: [:student, :admin, :teacher ]
-  validates :identification, presence: true
-  validates :username, presence: true
-  validates :number, presence: true
-  validates :email, presence: true
+  validates :identification, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :number, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   validates :borndate, presence: true
+  validates :role, presence: true
+  validates :type_user_id, presence: true
+  validates :jti, presence: true, uniqueness: true
+
+  belongs_to :type_user
 
 
   # Include default devise modules. Others available are:
